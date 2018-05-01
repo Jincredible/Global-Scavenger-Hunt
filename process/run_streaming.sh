55e7bd9a6e540f66bf4c7df1a52a38a7734de0d7
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-01 18:46:33 -0400

    minor shell edit

--- process/run_streaming.sh
+++ process/run_streaming.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This is the bash script to submit streaming.py to spark
 BASEDIR=$(dirname "$0")
-source process_cluster_config.sh
+source ${BASEDIR}/process_cluster_config.sh
 /usr/local/spark/bin/spark-submit --master spark://$process_master_hostname:7077 --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 --py-files ${BASEDIR}/streaming_config.py ${BASEDIR}/streaming.py
 
 
