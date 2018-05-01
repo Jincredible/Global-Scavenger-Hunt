2bc01604e708bd1d5eb89df847fdfbd2b7c5aa89
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-01 13:22:58 -0400

    minor edit for debugging

--- datastore/import_POI_to_redis.py
+++ datastore/import_POI_to_redis.py
@@ -17,12 +17,12 @@ def dataframe_from_csv(fn_csv):
 		df = pandas.read_csv(fn_csv, sep=',', header=0)
 	return df[[0,1]]
 
-def add_to_redis(df_in):
+def add_to_redis(df_in,str_in):
 	r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-	#for index, row in df_in.iterrows():
+	for index, row in df_in.iterrows():
 		#print("row['X']: ", str(row['X']), "row['Y']: ", str(row['Y']), "index: ", str(index+10000))
-		#r.geoadd('Boston',row['X'],row['Y'],index+10000)
+		r.geoadd('Boston',row['X'],row['Y'],str_in+str(index))
 
 	return
 
@@ -31,9 +31,9 @@ def add_to_redis(df_in):
 if __name__ == "__main__":
 	fn_csv_Boston = sys.argv[1]
 	fn_csv_Cambridge = sys.argv[2]
-	df_POI_01 = dataframe_from_csv(fn_csv_Boston)
-	df_POI_02 = dataframe_from_csv(fn_csv_Cambridge)
+	add_to_redis(dataframe_from_csv(fn_csv_Boston),'bos')
+	add_to_redis(dataframe_from_csv(fn_csv_Cambridge),'cam')
 	#df_POI = dataframe_from_csv(fn_csv_Boston).append(dataframe_from_csv(fn_csv_Cambridge),ignore_index=True)
-	print df_POI_01
-	print df_POI_02
+	#print df_POI_01
+	#print df_POI_02
 	#add_to_redis(df_POI)
