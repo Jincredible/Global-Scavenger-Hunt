f0d3a284d769d83dd0289c2ad3a79511acc83434
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-01 16:25:07 -0400

    removed streaming-config.py to add to gitignore. minor edit to streaming.py

--- process/streaming-config.py
+++
@@ -1,9 +0,0 @@
-# Streaming config file, read into streaming.py
-#Kafka variables
-KAFKA_TOPIC='user_data_01'
-KAFKA_SERVER_PORT=9092
-KAFKA_SERVER_DNS='ec2-52-202-238-209.compute-1.amazonaws.com:9092'
-
-#Redis variables
-REDIS_SERVER_DNS='ec2-18-208-17-22.compute-1.amazonaws.com'
-REDIS_SERVER_PORT=6379
--- process/streaming.py
+++ process/streaming.py
@@ -2,34 +2,26 @@
 # This python script is the main script for spark streaming. 
 # Here is the JSON format of the data from kafka:
 #
-# {"userid": text, 
-#  "time": timestamp, 
-#  "acc": float}
+# [user_id; timestamp; longitude; latitude; int(just_logged_in)]
 # The "acc" column is the acceleration of the user.
 #
 # The main tasks of thise script is the following:
 #
 # 1. Receive streaming data from kafka as a Dstream object 
-# 2. Take the original Dstream, calculate the window-average,
-#    and window-standard-deviation for each user and window,
-#    and produce a aggregated Dstream.
-# 3. Join the original Dstream with the aggregated Dstream 
-#    as a new Dstream
-# 4. Using the window-avg and window-std from the aggregated Dstream,
-#    label each record from the original Dstream as 'safe' or 'danger'
-# 5. Send the list of (userid, status) to rethinkDB
-# 6. Send all of the data to cassandra
+# 2. Take the original Dstream, get only the latest GPS message of each user
+# 3. Does the user exist in the redis database? 
+# 3a If Not: fetch N target locations for the user and store them into the
+#    user database
+# 3b If Yes: next step
+# 4. Calculate the distance between the user and each of the user locations.
+# 5. Is this distance between the user and a target location less than Y meters?
+# 5a If Yes: add the 
+
 #
 # The parameters
-# config.KAFKA_SERVERS: public DNS and port of kafka servers
-# config.CHECKPOINT_DIR: check point folder for window process  
-# config.ANOMALY_CRITERIA: if abs(data - avg) > config.ANOMALY_CRITERIA * std,
-#                          then data is an anomaly
-# config.RETHINKDB_SERVER: public DNS of the rethinkDB server
-# config.RETHINKDB_DB: name of the database in rethinkDB
-# config.RETHINKDB_TABLE: name of the table in rethinkDB
-# config.CASSANDRA_SERVERS: public DNS and port of cassandra servers
-# config.CASSANDRA_NAMESPACE: namespace for cassandra
-
-# were written in a separate "config.py".
+# streaming-config.KAFKA_TOPIC: name of kafka topic for upstream queue
+# streaming-config.KAFKA_DNS: public DNS and port for Kafka messages
+# streaming-config.REDIS_DNS: public DNS and port for Redis instance
+# streaming-config.REDIS_PASS: password for redis authentication
+# were written in a separate "streaming-config.py".
 ############################################################
