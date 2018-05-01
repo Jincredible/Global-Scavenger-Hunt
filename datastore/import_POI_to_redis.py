30e5423ab6a3c3fcdbdab286f265627f5515fa1a
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-01 13:15:26 -0400

    minor edit for debugging

--- datastore/import_POI_to_redis.py
+++ datastore/import_POI_to_redis.py
@@ -20,7 +20,7 @@ def dataframe_from_csv(fn_csv):
 def add_to_redis(df_in):
 	r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-	for index, row in df_in.iterrows():
+	#for index, row in df_in.iterrows():
 		#print("row['X']: ", str(row['X']), "row['Y']: ", str(row['Y']), "index: ", str(index+10000))
 		#r.geoadd('Boston',row['X'],row['Y'],index+10000)
 
@@ -31,6 +31,9 @@ def add_to_redis(df_in):
 if __name__ == "__main__":
 	fn_csv_Boston = sys.argv[1]
 	fn_csv_Cambridge = sys.argv[2]
-	df_POI = dataframe_from_csv(fn_csv_Boston).append(dataframe_from_csv(fn_csv_Cambridge),ignore_index=True)
-	print df_POI
-	add_to_redis(df_POI)
+	df_POI_01 = dataframe_from_csv(fn_csv_Boston)
+	df_POI_02 = dataframe_from_csv(fn_csv_Cambridge)
+	#df_POI = dataframe_from_csv(fn_csv_Boston).append(dataframe_from_csv(fn_csv_Cambridge),ignore_index=True)
+	print df_POI_01
+	print df_POI_02
+	#add_to_redis(df_POI)
