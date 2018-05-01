433f33ecc784e719f85817800c8e1cd8d8346fad
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-01 13:12:34 -0400

    minor edit for debugging

--- datastore/import_POI_to_redis.py
+++ datastore/import_POI_to_redis.py
@@ -21,7 +21,7 @@ def add_to_redis(df_in):
 	r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 	for index, row in df_in.iterrows():
-		print("row['X']: ", str(row['X']), "row['Y']: ", str(row['Y']), "index: ", str(index+10000))
+		#print("row['X']: ", str(row['X']), "row['Y']: ", str(row['Y']), "index: ", str(index+10000))
 		#r.geoadd('Boston',row['X'],row['Y'],index+10000)
 
 	return
@@ -32,4 +32,5 @@ if __name__ == "__main__":
 	fn_csv_Boston = sys.argv[1]
 	fn_csv_Cambridge = sys.argv[2]
 	df_POI = dataframe_from_csv(fn_csv_Boston).append(dataframe_from_csv(fn_csv_Cambridge),ignore_index=True)
+	print df_POI
 	add_to_redis(df_POI)
