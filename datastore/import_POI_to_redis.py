50944f52ed379b45079476dd4531b0522798d552
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-03 00:17:18 -0400

    slight edit to fix bug

--- datastore/import_POI_to_redis.py
+++ datastore/import_POI_to_redis.py
@@ -22,12 +22,7 @@ def dataframe_from_csv(fn_csv):
 		df = pandas.read_csv(fn_csv, sep=',', header=0)
 	return df[[0,1]]
 
-def add_to_redis(df_in,str_in,database_in):
-	r = redis.StrictRedis(host='localhost', port=config.REDIS_PORT, db=database_in, password=config.REDIS_PASS)
-
-	if config.REDIS_RESET:
-		print('flushing database:', database_in)
-		r.flushdb()
+def add_to_redis(r,df_in,str_in):
 
 	for index, row in df_in.iterrows():
 		#print("row[0]: ", str(row[0]), "row[1]: ", str(row[1]), "index: ", str(index))
@@ -41,8 +36,13 @@ if __name__ == "__main__":
 	fn_csv_Boston = sys.argv[1]
 	fn_csv_Cambridge = sys.argv[2]
 	database_num = sys.argv[3]
-	add_to_redis(dataframe_from_csv(fn_csv_Boston),'bos',database_num)
-	add_to_redis(dataframe_from_csv(fn_csv_Cambridge),'cam',database_num)
+	add_to_redis(r,dataframe_from_csv(fn_csv_Boston),'bos')
+	add_to_redis(r,dataframe_from_csv(fn_csv_Cambridge),'cam')
+
+	if config.REDIS_RESET:
+		r = redis.StrictRedis(host='localhost', port=config.REDIS_PORT, db=database_in, password=config.REDIS_PASS)
+		print('flushing database:', database_in)
+		r.flushdb()
 	#df_POI = dataframe_from_csv(fn_csv_Boston).append(dataframe_from_csv(fn_csv_Cambridge),ignore_index=True)
 	#print df_POI_01
 	#print df_POI_02
