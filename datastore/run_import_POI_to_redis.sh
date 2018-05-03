4f20443aebc1ff9c983f41ab9cfa211f2fa6519a
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-02 20:22:54 -0400

    slight edit to incorporate test database for redis in datastore shell script

--- datastore/run_import_POI_to_redis.sh
+++ datastore/run_import_POI_to_redis.sh
@@ -3,4 +3,5 @@
 #Start the redis server
 #/usr/local/redis/src/redis-server /usr/local/redis/redis.conf &
 BASEDIR=$(dirname "$0")
-python ${BASEDIR}/import_POI_to_redis.py ${BASEDIR}/POI_01.csv ${BASEDIR}/POI_01.csv
+DATABASE_NUM=7 #this value is 7 if it is a test database and 0 if it is production
+python ${BASEDIR}/import_POI_to_redis.py ${BASEDIR}/POI_01.csv ${BASEDIR}/POI_01.csv $(DATABASE_NUM)
