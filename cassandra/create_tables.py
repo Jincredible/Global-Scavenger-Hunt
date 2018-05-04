7b4481afabea32258c8e9b49897dd1d490f6d23a
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-03 22:02:24 -0400

    small error fixed

--- cassandra/create_tables.py
+++ cassandra/create_tables.py
@@ -17,4 +17,4 @@ session.execute('DROP TABLE IF EXISTS user_location;')
 session.execute('CREATE TABLE user_location (user_id text, time_stamp float, longitude float, latitude float, PRIMARY KEY (user_id, time_stamp)) WITH CLUSTERING ORDER BY (time_stamp ASC);')
 
 session.execute('DROP TABLE IF EXISTS user_target;')
-session.execute('CREATE TABLE user_target (user_id text, target_id text, time_stamp float, transaction_type int, PRIMARY KEY (user_id, target_id, time_stamp)) WITH CLUSTERING ORDER BY (time_stamp ASC);')
+session.execute('CREATE TABLE user_target (user_id text, target_id text, time_stamp float, transaction_type int, PRIMARY KEY (user_id, target_id, time_stamp));')
