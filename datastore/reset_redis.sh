0d98adcafb503c4e5d68cbbd06cc25ad41066798
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-07 20:00:49 -0400

    fixed minor bug

--- datastore/reset_redis.sh
+++ datastore/reset_redis.sh
@@ -4,4 +4,4 @@
 #/usr/local/redis/src/redis-server /usr/local/redis/redis.conf &
 BASEDIR=$(dirname "$0")
 DATABASE_NUM=7 #this value is 7 if it is a test database and 0 if it is production
-python ${BASEDIR}/import_POI_to_redis.py ${BASEDIR}/POI_01.csv ${BASEDIR}/POI_02.csv ${DATABASE_NUM}
+python ${BASEDIR}/reset_redis.py ${BASEDIR}/POI_01.csv ${BASEDIR}/POI_02.csv ${DATABASE_NUM}
