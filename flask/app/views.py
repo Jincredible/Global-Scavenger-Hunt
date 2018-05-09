029b7178ad131081f92735910f75a1481c939d14
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-09 15:46:08 -0400

    testing flask-in development

--- flask/app/views.py
+++ flask/app/views.py
@@ -10,12 +10,12 @@ from app import app
 
 
 # importing Cassandra modules from the driver we just installed
-from cassandra.cluster import Cluster
+#from cassandra.cluster import Cluster
 
 # Setting up connections to cassandra
 
 # Change the bolded text to your seed node public dns (no < or > symbols but keep quotations. Be careful to copy quotations as it might copy it as a special character and throw an error. Just delete the quotations and type them in and it should be fine. Also delete this comment line
-cluster = Cluster(['<seed-node-public-dns>'])
+#cluster = Cluster(['<seed-node-public-dns>'])
 
 
 
