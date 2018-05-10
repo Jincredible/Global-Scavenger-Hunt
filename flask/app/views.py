ad364aa6242110ca3629e7a409778af2fe68730a
Steven Jin <steven.jin.dartmouth@gmail.com>
2018-05-10 14:58:11 -0400

    working on front end

--- flask/app/views.py
+++ flask/app/views.py
@@ -8,6 +8,11 @@ from flask import jsonify
 
 from flask import render_template, request
 
+from flask import Flask
+
+from flask_googlemaps import GoogleMaps
+from flask_googlemaps import Map
+
 from app import app
 
 import global_config as config
@@ -19,19 +24,22 @@ from cassandra.cluster import Cluster
 cassandra_cluster = Cluster(config.CASSANDRA_DNS)
 cassandra_session = cassandra_cluster.connect(config.CASSANDRA_NAMESPACE)
 
+#initialize GoogleMaps extension
+GoogleMaps(app, key=config.GOOGLE_MAPS_API_KEY)
+
 @app.route('/')
 @app.route('/index')
 def index():
 	user = { 'nickname': 'Steven' } # sample user
 	mylist = [1,2,3,4]
 	return render_template("index.html", title = 'Home', user = user, mylist = mylist)
+
 @app.route('/base')
 def base():
 	return render_template("user_location_in.html")
 
 # Cassandra user_location table column names:
 # user_id | timestamp_produced | latitude | longitude | timestamp_spark
-
 
 @app.route("/base", methods=['POST'])
 def base_post():
@@ -44,8 +52,43 @@ def base_post():
 	jsonresponse = [{"user_id": x.user_id, "timestamp_produced": x.timestamp_produced, "latitude": x.latitude, "longitude": x.longitude, "timestamp_spark": x.timestamp_spark} for x in response_list]
 	return render_template("user_location_out.html", output=jsonresponse)
 
+@app.route('/realtime_example')
+def realtime_example():
+	return render_template("realtime_example.html")
 
-#can we keep this line of code or does views have to continuously run?
+@app.route('/map_example')
+def mapview():
+    # creating a map in the view
+    mymap = Map(
+        identifier="view-side",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[(37.4419, -122.1419)]
+    )
+    sndmap = Map(
+        identifier="sndmap",
+        lat=37.4419,
+        lng=-122.1419,
+        markers=[
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+             'lat': 37.4419,
+             'lng': -122.1419,
+             'infobox': "<b>Hello World</b>"
+          },
+          {
+             'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+             'lat': 37.4300,
+             'lng': -122.1400,
+             'infobox': "<b>Hello World from other place</b>"
+          }
+        ]
+    )
+    return render_template('map_example.html', mymap=mymap, sndmap=sndmap)
+
+
+
+#can we keep this line of code or does views have to continuously run? Answer: No.
 #cassandra_cluster.shutdown()
 
 
